97cfdfcfdfe3534d3117d37cc6dfdebd613433d2
Dan Bickford <dan@Dan-Bickfords-MacBook-Pro-2.local>
2012-10-01 20:10:54 -0400

    first commit

---
+++ tableFilter.js
@@ -0,0 +1,57 @@
+//First arg is class placed on table row to filter
+//Second arg is the id of the text field searching off of
+//Third arg is an array corresponding to column of table, ie first column(td) is 0, second is 1
+//By Dan Bickford
+
+var TableFilter = Class.create({
+	initialize: function(className, fieldId, rowArray){
+		this.className = className;
+		this.fieldId = fieldId;
+		this.listen();
+		this.rowArray = rowArray;
+	},
+	listen: function(){
+		var elem = $(this.fieldId);
+		if (elem.addEventListener){
+				 var me = this;
+		     elem.addEventListener ("keyup",function(){
+						me.filter(me.className, me.fieldId, me.rowArray)
+					},false);
+			
+		 }else if (elem.attachEvent){
+				 var me = this;
+		     elem.attachEvent ('onkeyup',me.filter(me.className, me.fieldId, me.rowArray));
+		 }
+	},
+	showAllResults: function(rows){
+		for(var i=0; i<rows.length; i++){
+			rows[i].style.display = ""
+		}
+	},
+	checkMatch: function(array, row){
+		var match = false;
+		for(var i=0; i<array.length; i++){
+			if(row.getElementsByTagName("td")[array[i]].innerHTML.toLowerCase().indexOf(text.toLowerCase()) != -1){
+				match = true
+			}
+		}
+		return match;
+	},
+	filter: function(className, fieldId, rowArray){
+		rows = document.getElementsByClassName(className);
+		text = $(fieldId).value;
+		this.showAllResults(rows);
+		for(var i=0; i<rows.length; i++){
+			match = this.checkMatch(rowArray, rows[i]);
+			if(text == "" || text == " "){
+				rows[i].style.display = ""
+			}else if(match){
+				rows[i].style.display = ""
+			}else{
+				rows[i].style.display = "none"
+			}
+		}
+	}
+})
+
+
